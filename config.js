9d41ab2cfc7c10a3e97d22bfd6ac03257d166dc2
Godwin Adah <adahgodwin224@gmail.com>
2023-05-29 23:55:29 +0100

    nmm

--- config.js
+++ config.js
@@ -1,16 +1,11 @@
-// const aws = require('aws-sdk');
 
-// aws = new aws.S3({
-//     secretKey: process.env.secretKey,
-//     'mongoUrl' : process.env.mongoUrl
-// })
-
-// module.exports = {
-//     "secretKey": process.env.secretKey,
-//     "mongoUrl": process.env.mongoUrl
-// }
 
 module.exports = {
-    "secretKey": "12345678",
-    "mongoUrl": "mongodb+srv://adahg3512:fgcot157813@cluster0.zgcth95.mongodb.net/?retryWrites=true&w=majority"
+    "secretKey": process.env.secretKey,
+    "mongoUrl": process.env.mongoUrl
 }
+
+// module.exports = {
+//     "secretKey": "12345678",
+//     "mongoUrl": "mongodb+srv://adahg3512:fgcot157813@cluster0.zgcth95.mongodb.net/?retryWrites=true&w=majority"
+// }
